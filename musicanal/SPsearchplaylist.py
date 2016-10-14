149b78bde5589af180fd01d65580e84fc21753e1
diakov2100 <diakov2100@gmail.com>
2016-10-14 22:18:18 +0300

    14.10

--- musicanal/SPsearchplaylist.py
+++ musicanal/SPsearchplaylist.py
@@ -1,57 +1,24 @@
+import pprint
+import sys
+import os
+import subprocess
 
-templateSource = document.getElementById('results-template').innerHTML,\
-    template = Handlebars.compile(templateSource),
-    resultsPlaceholder = document.getElementById('results'),
-    playingCssClass = 'playing',
-    audioObject = null;
+import spotipy
+import spotipy.util as util
 
-var fetchTracks = function (albumId, callback) {
-    $.ajax({
-        url: 'https://api.spotify.com/v1/albums/' + albumId,
-        success: function (response) {
-            callback(response);
-        }
-    });
-};
+def search():
 
-var searchAlbums = function (query) {
-    $.ajax({
-        url: 'https://api.spotify.com/v1/search',
-        data: {
-            q: query,
-            type: 'album'
-        },
-        success: function (response) {
-            resultsPlaceholder.innerHTML = template(response);
-        }
-    });
-};
+    username = 'diakov111'
+    playlist_name = 'Sport'
+    print("Usage: %s username playlist-name" % (sys.argv[0],))
 
-results.addEventListener('click', function (e) {
-    var target = e.target;
-    if (target !== null && target.classList.contains('cover')) {
-        if (target.classList.contains(playingCssClass)) {
-            audioObject.pause();
-        } else {
-            if (audioObject) {
-                audioObject.pause();
-            }
-            fetchTracks(target.getAttribute('data-album-id'), function (data) {
-                audioObject = new Audio(data.tracks.items[0].preview_url);
-                audioObject.play();
-                target.classList.add(playingCssClass);
-                audioObject.addEventListener('ended', function () {
-                    target.classList.remove(playingCssClass);
-                });
-                audioObject.addEventListener('pause', function () {
-                    target.classList.remove(playingCssClass);
-                });
-            });
-        }
-    }
-});
+    token = util.prompt_for_user_token(username)
 
-document.getElementById('search-form').addEventListener('submit', function (e) {
-    e.preventDefault();
-    searchAlbums(document.getElementById('query').value);
-}, false);
+    if token:
+        sp = spotipy.Spotify(auth=token)
+        sp.trace = False
+        playlists = sp.user_playlist_create(username, playlist_name)
+
+    else:
+        print("Can't get token for", username)
+
--- musicanal/main.py
+++ musicanal/main.py
@@ -2,25 +2,31 @@ import bpmdetect
 import os
 from pydub import AudioSegment
 import datetime
+import spotipy.util as util
+import SPsearchplaylist
+import spotipy
 
 
 
 if __name__ == '__main__':
+
     dir = 'd:/pymus'
     print(datetime.datetime.now())
-    namelist = os.listdir(dir)
+    SPsearchplaylist.search()
+    #namelist = os.listdir(dir)
 
-    namelist = filter(lambda x: x.endswith('.mp3'), namelist)
+    #namelist = filter(lambda x: x.endswith('.mp3'), namelist)
 
-    for name in namelist:
-        sound = AudioSegment.from_mp3(dir+'/'+name)
-        sound.export(dir+'/'+name[0:-3]+'wav', format="wav")
-        os.remove(dir + '/' + name)
+    #for name in namelist:
+     #   sound = AudioSegment.from_mp3(dir+'/'+name)
+     #   sound.export(dir+'/'+name[0:-3]+'wav', format="wav")
+      #  os.remove(dir + '/' + name)
 
-    namelist = os.listdir(dir)
+   # namelist = os.listdir(dir)
 
-    namelist =filter(lambda x: x.endswith('.wav'), namelist)
+#    namelist =filter(lambda x: x.endswith('.wav'), namelist)
 
-    for name in namelist:
-        bmpanaliz = bpmdetect.bpmdetect(dir+'/'+str(name))
-        print(datetime.datetime.now())
+ #   for name in namelist:
+  #      bmpanaliz = bpmdetect.bpmdetect(dir+'/'+str(name))
+   #     print(datetime.datetime.now())
+
