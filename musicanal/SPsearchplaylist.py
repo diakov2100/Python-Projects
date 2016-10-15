ac18e9a699d353a647f4cd142d06f1cc9a41e93a
diakov2100 <diakov2100@gmail.com>
2016-10-15 13:34:04 +0300

    15.10

--- musicanal/SPsearchplaylist.py
+++ musicanal/SPsearchplaylist.py
@@ -1,24 +1,30 @@
 import pprint
-import sys
-import os
-import subprocess
 
 import spotipy
+import sys
 import spotipy.util as util
+
+def show_tracks(tracks):
+    for i, item in enumerate(tracks['items']):
+        track = item['track']
+        print("   %d %32.32s %s" % (i, track['artists'][0]['name'], track['name']))
 
 def search():
 
-    username = 'diakov111'
-    playlist_name = 'Sport'
-    print("Usage: %s username playlist-name" % (sys.argv[0],))
-
-    token = util.prompt_for_user_token(username)
-
-    if token:
-        sp = spotipy.Spotify(auth=token)
-        sp.trace = False
-        playlists = sp.user_playlist_create(username, playlist_name)
-
+    if len(sys.argv) > 1:
+        search_str = sys.argv[1]
     else:
-        print("Can't get token for", username)
+        search_str = 'sport'
 
+    sp = spotipy.Spotify()
+    result = sp.search(search_str, 5, 0, 'playlist')
+    for playlist in result['playlists']['items']:
+        print(playlist['name'])
+        results = sp.user_playlist(playlist['owner']['id'], playlist['id'], fields="tracks,next"),# '94a636ebaa3f46d1af9c8dcf66858daf', '8b057cb6a70641969d3a5b706dc501d9')
+        tracks = results['tracks']
+        show_tracks(tracks)
+        while tracks['next']:
+            tracks = sp.next(tracks)
+            show_tracks(tracks)
+
+    #pprint.pprint(result.playlists)
